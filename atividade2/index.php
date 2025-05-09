f19b711798ddef851c6404ea1e0b50f17069d7cc
CaioAraujoo7 <todin13019@gmail.com>
2025-05-09 16:10:32 -0300

    Add files via upload

--- atividade2/index.php
+++ atividade2/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Minha Página</title>
+    <title>Minha Página (Fernando)</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
@@ -11,7 +11,7 @@
     <div class="container-fluid my-5">
         <?php
         $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 'home';
-        if ($pagina === 'home') { 
+        if ($pagina === 'home') {
             $pagePath = 'home.php';
         } else 
         $pagePath = "pages/formulario_{$pagina}/formulario.php";
